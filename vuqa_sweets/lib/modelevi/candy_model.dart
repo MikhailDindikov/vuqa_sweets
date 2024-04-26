7e25e5b30deea9653ff558d4fca5065b9945b965
MihailDundukov <misha2001du@gmail.com>
2024-04-26 23:10:24 +0300

    fix

--- vuqa_sweets/lib/modelevi/candy_model.dart
+++ vuqa_sweets/lib/modelevi/candy_model.dart
@@ -30,3 +30,192 @@ class CandyModel {
   factory CandyModel.ffffffffffff(fotka, nazv, tsennik, wasBought) =>
       CandyModel(fotka, nazv, tsennik, wasBought);
 }
+
+class CandyModel1 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel1(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel2 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel2(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel3 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel3(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel4 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel4(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel5 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel5(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel6 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel6(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel7 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel7(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel8 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel8(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel9 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel9(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel11 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel11(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel12 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel12(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel13 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel13(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel14 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel14(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel15 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel15(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel16 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel16(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel17 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel17(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel18 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel18(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel19 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel19(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel20 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel20(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel21 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel21(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
+
+class CandyModel22 {
+  final String fotka;
+  final String nazv;
+  final List<int> tsennik;
+  bool wasBought;
+
+  CandyModel22(this.fotka, this.nazv, this.tsennik, this.wasBought);
+}
