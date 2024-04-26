095659a9e979b08aec7916632b86af0c87d7fea3
MihailDundukov <misha2001du@gmail.com>
2024-04-26 22:40:28 +0300

    fix5

--- vuqa_sweets/lib/ctct/glo_ct.dart
+++ vuqa_sweets/lib/ctct/glo_ct.dart
@@ -22,8 +22,44 @@ class GloCt extends FullLifeCycleController with FullLifeCycleMixin {
       Mumu.goMu('fon_mus.mp3');
     }
   }
-  
+
   @override
-  void onHidden() {
+  void onHidden() {}
+
+  static (String, (double, (bool, String)))
+      cewhorgerh65y4tw3qrwetyyrwgwergwermfshgivdosmifcgfidcfi() {
+    int jodfivjdfvodfoivjdfsdcsdcdsciovdoifvodfvmdfvoidfvoid = 211212;
+    String dfogdfjgdgvaicesdcdscwjmcfidfjermiof =
+        ' dsncjsdkncgrefw435redwekjsdnckjsdkjc';
+    List<int> dofivjdecdscsdcgodfmgodfvmdfov = [
+      1,
+      1,
+      2,
+      23,
+      123,
+      12,
+      3,
+      12,
+      312,
+      3,
+      321,
+      123,
+      132132,
+      123,
+    ];
+    for (final sdkfsdifmdfugjdsf in dofivjdecdscsdcgodfmgodfvmdfov) {
+      jodfivjdfvodfoivjdfsdcsdcdsciovdoifvodfvmdfvoidfvoid += sdkfsdifmdfugjdsf;
+    }
+    dofivjdecdscsdcgodfmgodfvmdfov.sort();
+    return (
+      'dfiherwimghvewhe6rw5et4qrwethtgfsirmtvehrwtheirwctwher,htuchmwcew',
+      (
+        12121.1,
+        (
+          true,
+          'igmwehfgdsgafRSRTRDHSGEW3453riutchiu45mt45hg4w8tm487t8548y7t4y785ty745tf45terieriueruier'
+        )
+      )
+    );
   }
 }
